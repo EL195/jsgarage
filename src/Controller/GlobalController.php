763dd7f3459ab3363130b3d68c6e65783f1b2e28
Js <jsnkodia@gmail.com>
2021-09-27 17:03:43 +0200

    2e commit

--- src/Controller/GlobalController.php
+++ src/Controller/GlobalController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Utilisateur;
+use App\Form\InscriptionType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class GlobalController extends AbstractController
 {
@@ -14,5 +19,39 @@ class GlobalController extends AbstractController
     public function index()
     {
         return $this->render('global/accueil.html.twig');
+    }
+
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function inscription(Request $request, EntityManagerInterface $om, UserPasswordHasherInterface $passwordHasher)
+    {
+        $utilisateur = new Utilisateur();
+        $form = $this->createForm(InscriptionType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {            
+            $passwordCrypte = $passwordHasher->hashPassword($utilisateur, $utilisateur->getPassword());
+            $utilisateur->setPassword($passwordCrypte);
+            $utilisateur->setRoles("ROLE_USER");
+            $om->persist($utilisateur);
+            $om->flush();
+            return $this->redirectToRoute("accueil");
+        }
+
+        
+        return $this->render('global/inscription.html.twig',
+        [
+            "form" => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        return $this->render('global/inscription.html.twig');
     }
 }
